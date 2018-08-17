fcff68731e1d1e7c80e6838ffcd253e95566a8b9
Cayo <cayohollanda@hotmail.com>
2018-08-17 01:06:39 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -34,10 +34,7 @@ export class CategoriasPage {
           this.items = response;
         },
 
-        error => {
-          console.log(error);
-        }
-      );
+        error => {});
   }
 
 }
